4618a7826d559a4c708919fcfecff52affe16534
Charlie Batchelor <cbatchel@dunegpvm12.fnal.gov>
2023-03-23 07:44:07 -0500

    Fix replay app.

--- python/trigger/replay_tp_to_chain/__main__.py
+++ python/trigger/replay_tp_to_chain/__main__.py
@@ -29,7 +29,6 @@ import click
 @click.option('--trigger-candidate-plugin', default='TriggerCandidateMakerPrescalePlugin', help="Trigger candidate algorithm plugin")
 @click.option('--trigger-candidate-config', default='dict(prescale=100)', help="Trigger candidate algorithm config (string containing python dictionary)")
 @click.option('-l', '--number-of-loops', default='-1', help="Number of times to loop over the input files (-1 for infinite)")
-#@click.option('-h', '--hardware-map', type=click.Path(exists=True, dir_okay=False), help="Hardware map file.")
 @click.argument('json_dir', type=click.Path())
 def cli(slowdown_factor, input_file, trigger_activity_plugin, trigger_activity_config, trigger_candidate_plugin, trigger_candidate_config, number_of_loops, json_dir):
     """
