39d65fe8be008128dc06ba49c8b62a431f086424
Eric Flumerfelt <eflumerf@fnal.gov>
2024-10-02 14:16:43 -0500

    Modify TCMakers so that the TriggerCandidates they create can be sent through the DataSubscriberModule data path.

--- plugins/RandomTCMakerModule.hpp
+++ plugins/RandomTCMakerModule.hpp
@@ -26,7 +26,6 @@
 #include "iomanager/Sender.hpp"
 #include "utilities/TimestampEstimator.hpp"
 #include "triggeralgs/TriggerCandidate.hpp"
-#include "trigger/TCWrapper.hpp"
 #include "trigger/opmon/randomtcmaker_info.pb.h"
 
 #include <memory>
@@ -82,7 +81,7 @@ private:
 
   // Queue sources and sinks
   std::shared_ptr<iomanager::ReceiverConcept<dfmessages::TimeSync>> m_time_sync_source;
-  std::shared_ptr<iomanager::SenderConcept<trigger::TCWrapper>> m_trigger_candidate_sink;
+  std::shared_ptr<iomanager::SenderConcept<triggeralgs::TriggerCandidate>> m_trigger_candidate_sink;
 
   //randomtriggercandidatemaker::Conf m_conf;
   const appmodel::RandomTCMakerConf* m_conf;
