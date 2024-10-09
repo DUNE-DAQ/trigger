22f6687bb882a25ba9f92f52cac13923b07293af
MRiganSUSX <M.Rigan@sussex.ac.uk>
2024-10-09 16:16:42 +0200

    latency: making micros the default

--- include/trigger/Latency.hpp
+++ include/trigger/Latency.hpp
@@ -20,14 +20,14 @@ namespace dunedaq {
   namespace trigger {
 
     class Latency {
-      using latency = double;
+      using latency = uint64_t;
 
       public:
           // Enumeration for selecting time units
-          enum class TimeUnit { Milliseconds = 1, Microseconds = 2 };
+          enum class TimeUnit { Microseconds = 1, Milliseconds = 2 };
 
           // Constructor with optional time unit selection (defaults to Milliseconds)
-          Latency(TimeUnit time_unit = TimeUnit::Milliseconds)
+          Latency(TimeUnit time_unit = TimeUnit::Microseconds)
               : m_latency_in(0), m_latency_out(0), m_time_unit(time_unit), clock_freq(62500000) {
 
               setup_conversion();
@@ -41,7 +41,7 @@ namespace dunedaq {
           }
 
           // Function to get the current system time in ms or ns based on time unit
-          uint64_t get_current_system_time() const {
+          latency get_current_system_time() const {
               return m_timestamp_estimator->get_timestamp_estimate();
           }
 
@@ -50,7 +50,6 @@ namespace dunedaq {
               uint64_t current_time = get_current_system_time();
               uint64_t diff = (current_time >= latency) ? (current_time - latency) : 0;
               m_latency_in.store( diff * m_clock_ticks_conversion );
-              TLOG() << static_cast<int>(m_time_unit) << " " << current_time << " " << latency << " " << diff << " " << m_latency_in.load();
           }
 
           // Function to update latency_out
@@ -72,21 +71,19 @@ namespace dunedaq {
 
       private:
           void setup_conversion() {
-              if (m_time_unit == TimeUnit::Milliseconds) {
-                  m_clock_ticks_conversion = 16 * 1e-6; // For milliseconds
+              if (m_time_unit == TimeUnit::Microseconds) {
+                  m_clock_ticks_conversion = 16 * 1e-3;
               } else {
-                  m_clock_ticks_conversion = 16 * 1e-3; // For microseconds
+                  m_clock_ticks_conversion = 16 * 1e-6;
               }
           }
 
           std::atomic<latency> m_latency_in;  // Member variable to store latency_in
           std::atomic<latency> m_latency_out; // Member variable to store latency_out
           TimeUnit m_time_unit;               // Member variable to store the selected time unit (ms or ns)
-	  latency m_clock_ticks_conversion;   // Conversion factor from ticks to the selected time unit
+	  double m_clock_ticks_conversion;   // Conversion factor from ticks to the selected time unit
 	  std::unique_ptr<utilities::TimestampEstimatorBase> m_timestamp_estimator;
           latency clock_freq;
-          // Function pointer or lambda for conversion based on time unit
-          std::function<uint64_t(uint64_t)> m_convert_latency;
     };
 
   } // namespace trigger
