aff5b2975bc34a2785cd2b6a39d045edc7b115d4
Kurt Biery <biery@daq.fnal.gov>
2026-01-12 12:41:49 -0600

    Improved the consistency of the handling of the banner line in the integtests (printout of the name of each config/test).

--- integtest/change_rate_test.py
+++ integtest/change_rate_test.py
@@ -1,5 +1,6 @@
 import pytest
-import copy
+import os
+import re
 
 import integrationtest.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
@@ -128,6 +129,16 @@ nanorc_command_list = (
 
 
 def test_nanorc_success(run_nanorc):
+    # print the name of the current test
+    current_test = os.environ.get("PYTEST_CURRENT_TEST")
+    match_obj = re.search(r".*\[(.+)-run_.*rc.*\d].*", current_test)
+    if match_obj:
+        current_test = match_obj.group(1)
+    banner_line = re.sub(".", "=", current_test)
+    print(banner_line)
+    print(current_test)
+    print(banner_line)
+
     # Check that nanorc completed correctly
     assert run_nanorc.completed_process.returncode == 0
 
