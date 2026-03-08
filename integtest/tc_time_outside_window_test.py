61f9528f4190699f4bf803c4b7555fd6f5637e31
Kurt Biery <biery@daq.fnal.gov>
2026-03-08 12:32:53 -0500

    In the integtests, renamed the drunc_config session parameter to config_session_name to help make its meaning more clear. Also removed obsolete references to data_rate_slowdown_factor.

--- integtest/tc_time_outside_window_test.py
+++ integtest/tc_time_outside_window_test.py
@@ -11,7 +11,6 @@ pytest_plugins = "integrationtest.integrationtest_drunc"
 
 # Values that help determine the running conditions
 number_of_data_producers = 1
-data_rate_slowdown_factor = 1  # 10 for ProtoWIB/DuneWIB
 run_duration = 20  # seconds
 readout_window_time_before = 1000
 readout_window_time_after = 1001
@@ -83,17 +82,10 @@ object_databases = ["config/daqsystemtest/integrationtest-objects.data.xml"]
 conf_dict = data_classes.drunc_config()
 conf_dict.dro_map_config.n_streams = number_of_data_producers
 conf_dict.op_env = "integtest"
-conf_dict.session = "tcoutsidewindow"
+conf_dict.config_session_name = "tcoutsidewindow"
 conf_dict.tpg_enabled = False
 conf_dict.fake_hsi_enabled = True
 
-conf_dict.config_substitutions.append(
-    data_classes.attribute_substitution(
-        obj_id=conf_dict.session,
-        obj_class="Session",
-        updates={"data_rate_slowdown_factor": data_rate_slowdown_factor},
-    )
-)
 conf_dict.config_substitutions.append(
     data_classes.attribute_substitution(obj_class="LatencyBuffer", updates={"size": 50000})
 )
