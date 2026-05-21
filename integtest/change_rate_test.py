6ba951dfb02aa434deeb6e7cc2ef1b32dae5bf6d
Kurt Biery <biery@daq.fnal.gov>
2026-05-21 12:08:26 -0500

    Modified the integtest to support user-requested deletion of HDF5 files at the end of the test.

--- integtest/change_rate_test.py
+++ integtest/change_rate_test.py
@@ -4,8 +4,8 @@ import re
 
 import integrationtest.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
-import integrationtest.basic_checks as basic_checks
 import integrationtest.data_classes as data_classes
+import integrationtest.utility_functions as utility_functions
 from integrationtest.verbosity_helper import IntegtestVerbosityLevels
 
 import functools
@@ -120,7 +120,7 @@ dunerc_command_list = (
 
 def test_dunerc_success(run_dunerc, caplog):
     # check for run control success, problems during pytest setup, etc.
-    basic_checks.basic_checks(run_dunerc, caplog, print_test_name=False)
+    utility_functions.basic_checks(run_dunerc, caplog, print_test_name=False)
 
 
 def test_log_files(run_dunerc):
@@ -155,3 +155,7 @@ def test_data_files(run_dunerc):
             assert data_file_checks.check_fragment_sizes(
                 data_file, fragment_check_list[jdx]
             )
+
+
+def test_cleanup(run_dunerc):
+    utility_functions.remove_hdf5_files_if_requested(run_dunerc, this_test_requests_hdf5_file_removal=False)
